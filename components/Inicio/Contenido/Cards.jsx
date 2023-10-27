9314dd8271a034e360a0a37b47f1a701c2831667
CDRojasRamirez <cdanielrap@gmail.com>
2023-10-27 17:39:14 -0500

    Optimizando responsive en cards del inicio

--- components/Inicio/Contenido/Cards.jsx
+++ components/Inicio/Contenido/Cards.jsx
@@ -193,9 +193,10 @@ const Cards = ({ selectedAtencion, textSearch, setResultadosEncontrados, setNoth
 
   const breakpointColumnsObj = {
     default: 5, // Número de columnas por defecto
-    1200: 4, // Cambiar a 4 columnas en pantallas de 1200px o menos
-    992: 3, // Cambiar a 3 columnas en pantallas de 992px o menos
-    768: 2, // Cambiar a 2 columnas en pantallas de 768px o menos
+    1500: 4,
+    1200: 3, // Cambiar a 4 columnas en pantallas de 1200px o menos
+    992: 2, // Cambiar a 3 columnas en pantallas de 992px o menos
+    768: 1, // Cambiar a 2 columnas en pantallas de 768px o menos
     576: 1, // Cambiar a 1 columna en pantallas de 576px o menos
   };
   
